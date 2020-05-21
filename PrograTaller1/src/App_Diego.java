1cc8e6a58ba14d18d8571d18f57c5df5926cd064
diegovera96 <52257718+diegovera96@users.noreply.github.com>
2020-05-20 21:42:37 -0400

    el de socios y parte de prestamos

--- PrograTaller1/src/App_Diego.java
+++ PrograTaller1/src/App_Diego.java
@@ -10,6 +10,9 @@ public class App_Diego {
 			Registro reg = socios.getRegistro();
 			cant_Lineas_Socios++;	//suma la cantidad de lineas para poder determinar el largo que tendrán los vectores del archivo Socios.txt
 			StdOut.println(cant_Lineas_Socios); //testing
+			if(!"Socios.txt".isEmpty()) {
+
+			}
 		}
 		
 		//Se crean los vectores del archivo Socios.txt
@@ -23,7 +26,7 @@ public class App_Diego {
 		socios = new ArchivoEntrada("Socios.txt");	//Lee el archivo Socios.txt y designa cada palabra o frase separada por comas como una variable
 		while(!socios.isEndFile()) {
 			Registro reg = socios.getRegistro();	//Se agrego un .replaceAll(" ", "") para eliminar los espacios posibles entre datos
-			String rut = reg.getString().replaceAll(" ", "");
+			String rut = reg.getString().replaceAll(" ", "").replaceAll("-", "");
 			String nombre = reg.getString().replaceAll(" ", "");
 			String apellido = reg.getString().replaceAll(" ", "");
 			String activo_1 = reg.getString().replaceAll(" ", "");
@@ -42,18 +45,19 @@ public class App_Diego {
 			vec_Activo1[aux_Socios] = activo_1;
 			vec_Activo2[aux_Socios] = activo_2;
 			
-			StdOut.println(vec_Rut[aux_Socios]); //testing para ver si funciona 
-			StdOut.println(vec_Activo2[aux_Socios]); //same
+			//StdOut.println(vec_Rut[aux_Socios]); //testing para ver si funciona 
+			//StdOut.println(vec_Activo2[aux_Socios]); //same
 			aux_Socios++;
+
 		}
-		StdOut.println("---------------------------------------------------------------");
+		//StdOut.println("---------------------------------------------------------------");
 		
 		int cant_Lineas_Libros = 0;
 		ArchivoEntrada libros = new ArchivoEntrada("Libros.txt");
 		while(!libros.isEndFile()) {
 			Registro reg = libros.getRegistro();
 			cant_Lineas_Libros++;	//Suma para obtener la cantidad de lineas que tiene el archivo Libros.txt
-			StdOut.println(cant_Lineas_Libros); //testing
+			//StdOut.println(cant_Lineas_Libros); //testing
 		}
 		
 		String vec_CodigoLibro[] = new String[cant_Lineas_Libros];
@@ -82,11 +86,11 @@ public class App_Diego {
 			vec_ApellidoAutor[aux_Libros] = apell_Autor;
 			vec_Categoria[aux_Libros] = categoria;
 			//vec_SumaLibros[aux_Libros] = ;
-			StdOut.println(vec_CodigoLibro[aux_Libros]); //testing
+			//StdOut.println(vec_CodigoLibro[aux_Libros]); //testing
 			aux_Libros++;
 		}
 		
-		StdOut.println("---------------------------------------------------------------");
+		//StdOut.println("---------------------------------------------------------------");
 
 		
 		int cant_Lineas_Prestamos = 0;
@@ -94,7 +98,7 @@ public class App_Diego {
 		while(!prestamos.isEndFile()) {
 			Registro reg = prestamos.getRegistro();
 			cant_Lineas_Prestamos++; //Suma que determinará la cantidad de lineas que tiene el archivo Prestamos.txt
-			StdOut.println(cant_Lineas_Prestamos);
+			//StdOut.println(cant_Lineas_Prestamos);
 		}
 		
 		String vec_CodigoPrestamo[] = new String[cant_Lineas_Prestamos];
@@ -120,16 +124,17 @@ public class App_Diego {
 			vec_RutSocio[aux_Prestamos] = rut_Socio;
 			vec_FechaPrestamo[aux_Prestamos] = fecha_Prestamo;
 			vec_FechaDevolucion[aux_Prestamos] = fecha_Devolucion;
-			StdOut.println(vec_CodigoPrestamo[aux_Prestamos]); //testing
+			//StdOut.println(vec_CodigoPrestamo[aux_Prestamos]); //testing
 
 			aux_Prestamos++;
 		}
 		
-		StdOut.println("---------------------------------------------------------------");
+		//StdOut.println("---------------------------------------------------------------");
 
 		
 		//Se inicia el primer menu del programa con 4 posibles opciones
 		int opcionRequerida_1 = 0;
+		int opcionRequerida_2 = 0;
 		while(opcionRequerida_1 != 4) {
 			
 			opcionRequerida_1 = 0;
@@ -155,13 +160,167 @@ public class App_Diego {
 		
 			switch(opcionRequerida_1) {
 			case 1:
+				while(opcionRequerida_2 != 4) {
+					
+					opcionRequerida_2 = 0;
+					do {
+						try {
+							StdOut.println("[1] Lista de socios activos.");
+							StdOut.println("[2] Cantidad de socios no activos.");
+							StdOut.println("[3] Buscar estado de socio por RUT.");
+							StdOut.println("[4] Volver.");	
+							
+							StdOut.println("Ingrese una opcion.");
+							String opcionIngresada_1 = StdIn.readString();
+							opcionRequerida_2 = Integer.parseInt(opcionIngresada_1);
+							if(opcionRequerida_2 < 0 || opcionRequerida_2 > 4) {	//Se delimitan las opciones ingresadas por el usuario
+								StdOut.println("Opcion no valida.");
+							}
+							
+						}catch(NumberFormatException exc) {
+							StdOut.println("Se ha ingresado una opcion no valida");
+						}
+					}while(opcionRequerida_2 == 0);
+					
+					switch(opcionRequerida_2) {
+					case 1:
+						desplegar_Activos(cant_Lineas_Socios, vec_Activo2, vec_Nombre, vec_Apellido);
+						break;
+					case 2:
+						StdOut.print("La cantidad de socios no activos es de: ");
+						int contador_NoActivos = 0;
+						for(int i = 0; i < cant_Lineas_Socios; i++) { //for que permite recorrer el vector no activos y desplegar su cantidad no activa
+							if(vec_Activo2[i] == false) {
+								contador_NoActivos++;
+							}
+						}
+						StdOut.println(contador_NoActivos + " Usuarios no activos.");
+						break;
+					case 3:
+						String rut_Ingresado = "";
+						int rut_Requerido = 0;
+						try {
+							StdOut.println("Ingrese un rut sin puntos.");
+							StdOut.println("Si termina en K, reemplace por un 0.");
+							rut_Ingresado = StdIn.readString().replaceAll("-", "").replaceAll("k", "0").replaceAll("K", "0"); //reemplaza por un 0 a los ruts terminados en k ingresados por el usuario.
+							rut_Requerido = Integer.parseInt(rut_Ingresado);
+
+						}catch(NumberFormatException exc) {
+							StdOut.println("Se ha ingresado un rut con formato no valida"); //manejo de excepcion para un rut no valido
+						}
+						
+						int i = 0;
+						int aux = 0;
+						Boolean rut_Encontrado = false;
+						for(i = 0; i < cant_Lineas_Socios; i++) {
+							if(Integer.parseInt(vec_Rut[i].replaceAll("k", "0").replaceAll("K", "0")) == (rut_Requerido)) { //reemplaza por un 0 a los ruts terminados en k del vector vec_Rut
+								rut_Encontrado = true;
+								aux = i;
+							}
+						}
+						if(rut_Encontrado == true) {
+							if(vec_Activo2[aux] == true) {
+								StdOut.println("El estado actual del rut " + vec_Rut[aux] + " es Activo.");
+							}
+							else {
+								StdOut.println("El estado actual del rut " + vec_Rut[aux] + " es No Activo.");
+							}
+						}
+						else {
+							StdOut.println("Rut no encontrado.");
+						}
+						break;
+					case 4:
+						break;
+					}
+				}
 				break;
 				
 			case 2:
+				/*while(opcionRequerida_2 != 3) {
+					
+					opcionRequerida_2 = 0;
+					do {
+						try {
+							StdOut.println("[1] Lista de libros.");
+							StdOut.println("[2] Buscar libro por codigo.");
+							StdOut.println("[3] Volver.");
+
+							StdOut.println("Ingrese una opcion.");
+							String opcionIngresada2 = StdIn.readString();
+							opcionRequerida_2 = Integer.parseInt(opcionIngresada2);
+							if(opcionRequerida_2 < 0 || opcionRequerida_2 > 3) {
+								StdOut.println("Ingrese una opcion valida");
+							}
+						
+						}catch(NumberFormatException exc) {
+							StdOut.println("Se ha ingresado una opcion no valida");
+						}
+					}while(opcionRequerida_2 == 0);
+					
+					switch(opcionRequerida_2){
+					case 1:
+						
+					case 2:
+						
+					case 3:
+						break;
+					}
+
+				}*/
+				
+				//libros
 				break;
 				
 			case 3:
+				while(opcionRequerida_2 != 5) {
+					opcionRequerida_2 = 0;
+					do {
+						try {
+							StdOut.println("[1] Lista de libros prestados.");
+							StdOut.println("[2] Cantidad de libros prestados.");
+							StdOut.println("[3] Libro mas solicitado.");
+							StdOut.println("[4] Libro menos solicitado.");
+							StdOut.println("[5] Volver.");
+
+							StdOut.println("Ingrese una opcion.");
+							String opcionIngresada2 = StdIn.readString();
+							opcionRequerida_2 = Integer.parseInt(opcionIngresada2);
+							if(opcionRequerida_2 < 0 || opcionRequerida_2 > 5) {
+								StdOut.println("Ingrese una opcion valida");
+							}
+						}catch(NumberFormatException exc) {
+							StdOut.println("Se ha ingresado una opcion no valida");
+						}
+					}while(opcionRequerida_2 == 0);
+					
+					switch(opcionRequerida_2) {
+					case 1:
+						//Lista de libros prestados.
+						break;
+					case 2:
+						//Cantidad de libros prestados.
+						break;
+					case 3:
+						//Libro mas solicitado.
+						break;
+					case 4:
+						//Libro menos solicitado.
+						break;
+					case 5:
+						break; //volver
+					}
+				}
 				break;
+			}
+		}
+	}
+	
+	public static void desplegar_Activos(int cant_Lineas_Socios, Boolean vec_Activo2[], String vec_Nombre[], String vec_Apellido[]) { //metodo que permite despelgar los usuarios activos
+		StdOut.println("Los usuarios actualemente activos son:");
+		for(int i = 0; i < cant_Lineas_Socios; i++) {
+			if(vec_Activo2[i] == true) {
+				StdOut.println("Nombre: "+ vec_Nombre[i] +" "+ vec_Apellido[i]);
 			}
 		}
 	}
